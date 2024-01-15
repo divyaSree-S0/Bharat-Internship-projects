fdc31824a950f12a5183933f3f821ebef16558b8
divyaSree-S0 <divyasrees.cse.41@gmail.com>
2024-01-16 00:36:20 +0530

    Creating registration app

--- Registration form/src/registration_form/settings.py
+++ Registration form/src/registration_form/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'registration',
+    'pymongo',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'registration_form.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,6 +80,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'mongodb':{
+        'ENGINE': 'pymongo',
+        'NAME': 'Bharat_intern',
+        'HOST': 'localhost',
+        'PORT': 27017,
+        'ENFORCE_SCHEMA':False,
     }
 }
 
