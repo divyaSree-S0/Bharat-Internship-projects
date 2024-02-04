78ce4eea9a22d6bcef8db67cb90bc06898c87d4e
divyaSree-S0 <divyasrees.cse.41@gmail.com>
2024-02-04 21:32:50 +0530

    added db

--- Registration form/node js/index.js
+++ Registration form/node js/index.js
@@ -1,13 +1,48 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app=express();
 const port = 3000;
 
+mongoose.connect('mongodb://localhost:27017/', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    username: String,
+    password: String,
+    email: String,
+    dob: Date
+  });
+
+const User = mongoose.model('User', userSchema);
+
 app.set('view engine','ejs');
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/',(req,res)=>
 res.render('registration'));
 
+app.post('/', async(req,res)=>{
+    try{
+        const newUser = new User({
+            name: req.body.name,
+            username: req.body.username,
+            password: req.body.password,
+            email: req.body.email,
+            dob: req.body.dob
+        });
+        await newUser.save();
+        res.render('registration', { registrationSuccess: true });
+        // res.send('Thank you for registering');
+        console.log(newUser,'saved');
+    }
+    catch(err){
+        console.log(err);
+        res.render('registration', { registrationSuccess: false });
+        // res.send('Sorry! Something went wrong.Please register again.');
+    }
+});
+
 app.listen(port,()=> {
-    console.log('Server is running at http://localhost:3000');
+    console.log(`Server is running at http://localhost:${port}`);
 })
